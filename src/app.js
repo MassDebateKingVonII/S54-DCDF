e7fba0b193fd73317c196eed3d5aaf8d2b0caccb
MassDebateKingVonII <bhbboonhowboy@gmail.com>
2025-09-24 02:56:37 +0800

    Fix

--- src/app.js
+++ src/app.js
@@ -27,44 +27,44 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-// // Main index page
-// app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname, "..", "views", "index.html"));
-// });
+// Main index page
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+});
 
-// // Year 1, Sem 1
-// app.get("/year1/sem1/", (req, res) => {
-//     res.sendFile(path.join(__dirname, "..", "views", "year_one", "semester_one", "navigation.html"));
-// });
+// Year 1, Sem 1
+app.get("/year1/sem1/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "views", "year_one", "semester_one", "navigation.html"));
+});
 
-// // Year 1, Sem 2
-// app.get("/year1/sem2/", (req, res) => {
-//     res.sendFile(path.join(__dirname, "..", "views", "year_one", "semester_two", "navigation.html"));
-// });
+// Year 1, Sem 2
+app.get("/year1/sem2/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "views", "year_one", "semester_two", "navigation.html"));
+});
 
 
-// // FOP
-// app.get("/fop/:page", (req, res) => {
-//     const page = req.params.page;
-//     const validPages = ["intro", "tut", "notes", "papers"];
+// FOP
+app.get("/fop/:page", (req, res) => {
+    const page = req.params.page;
+    const validPages = ["intro", "tut", "notes", "papers"];
 
-//     if (validPages.includes(page)) {
-//         res.sendFile(path.join(__dirname, "..", "views", "year_one", "semester_one", "fop", `${page}.html`));
-//     } else {
-//         res.status(404).json({
-//             message: "Not Found"
-//         })
-//     }
-// });
+    if (validPages.includes(page)) {
+        res.sendFile(path.join(__dirname, "..", "views", "year_one", "semester_one", "fop", `${page}.html`));
+    } else {
+        res.status(404).json({
+            message: "Not Found"
+        })
+    }
+});
 
-// app.get("/fop/tut/:tutorialNo", (req, res) => {
-//     const tutorialNo = req.params.tutorialNo;
-//     const validtutorialNo = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+app.get("/fop/tut/:tutorialNo", (req, res) => {
+    const tutorialNo = req.params.tutorialNo;
+    const validtutorialNo = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-//     if (validtutorialNo.includes(tutorialNo)) {
-//         res.sendFile(path.join(__dirname, "..", "views", "year_one", "semester_one", "fop", "tutorial", `${tutorialNo}.html`));
-//     }
-// });
+    if (validtutorialNo.includes(tutorialNo)) {
+        res.sendFile(path.join(__dirname, "..", "views", "year_one", "semester_one", "fop", "tutorial", `${tutorialNo}.html`));
+    }
+});
 
 module.exports = app;
 
